ac1a92fa90f27fdb850c042e0225d573b3a6d9ea
arimura <kotaro_arimura@voyagegroup.com>
2021-11-09 17:28:21 +0900

    init

---
+++ ManualInstalling/ManualInstalling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManualInstalling
+//
+//  Created by k-arimura on 2021/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
